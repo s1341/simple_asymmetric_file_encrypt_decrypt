bedc5cc0667b6625307077ba12f7c4ae490df320
s1341 <github@shmarya.net>
2013-12-29 10:24:18 +0200

    add rsa functions, cleanup

--- encrypt_decrypt.sh
+++ encrypt_decrypt.sh
@@ -1,3 +1,54 @@
+function rsa_encrypt () {
+    args=( $@ )
+    pubkey="${args[0]}"
+    plaintext_file="${args[1]}"
+    ciphertext_file="${args[2]}"
+
+    openssl rsautl -encrypt -pubin -inkey ${pubkey} -in ${plaintext_file} -out ${ciphertext_file}
+}
+
+function rsa_decrypt () {
+    args=( $@ )
+    privkey="${args[0]}"
+    ciphertext_file="${args[1]}"
+    plaintext_file="${args[2]}"
+
+    openssl rsautl -decrypt -inkey ${privkey} -in ${ciphertext_file} -out ${plaintext_file}
+}
+
+function aes_encrypt () {
+    args=( $@ )
+    keyfile="${args[0]}"
+    ciphertext_file="${args[1]}"
+    plaintext_files="${args[@]:2}"
+
+    if [[ ! -e ${keyfile} ]];
+    then
+        echo "[encrypt] missing keyfile (${keyfile}). make sure to call generate_keyfile() first"
+        return 1
+    fi
+    # compress and aes encrypt file with keyfile
+    tar cz ${plaintext_files[@]} | openssl enc -aes-256-cbc -salt -out ${ciphertext_file} -pass file:${keyfile}
+}
+
+function aes_decrypt () {
+    args=( $@ )
+    keyfile="${args[0]}"
+    ciphertext_file="${args[1]}"
+    plaintext_files="${args[@]:2}"
+
+    if [[ ! -e ${keyfile} ]];
+    then
+        echo "[decrypt] missing keyfile (${keyfile}). make sure to call decrypt_keyfile() first"
+        return 1
+    fi
+
+    # decrypt and decompress file with keyfiles
+    openssl enc -d -aes-256-cbc -in ${ciphertext_file} -pass file:${keyfile} | tar xz "${plaintext_files[@]}"
+}
+
+
+
 function generate_keyfile () {
     args=( $@ )
     pubkey="${args[0]}"
@@ -5,7 +56,7 @@ function generate_keyfile () {
     enckeyfile="${args[2]}"
 
     openssl rand 256 > ${keyfile}
-    openssl rsautl -encrypt -pubin -inkey ${pubkey} -in ${keyfile} -out ${enckeyfile}
+    rsa_encrypt ${pubkey} ${keyfile} ${enckeyfile}
 }
 
 function decrypt_keyfile () {
@@ -14,22 +65,7 @@ function decrypt_keyfile () {
     enckeyfile="${args[1]}"
     keyfile="${args[2]}"
 
-    openssl rsautl -decrypt -inkey ${privatekey} -in ${enckeyfile} -out ${keyfile}
-}
-
-function aesencrypt () {
-    args=( $@ )
-    keyfile="${args[0]}"
-    encfilename="${args[1]}"
-    files="${args[@]:2}"
-
-    if [[ ! -e ${keyfile} ]];
-    then
-        echo "[encrypt] missing keyfile (${keyfile}). make sure to call generate_keyfile() first"
-        return 1
-    fi
-    # compress and aes encrypt file with keyfile
-    tar cz ${files[@]} | openssl enc -aes-256-cbc -salt -out ${encfilename} -pass file:${keyfile}
+    rsa_decrypt ${privatekey} ${enckeyfile} ${keyfile}
 }
 
 function encrypt () {
@@ -41,27 +77,11 @@ function encrypt () {
     files="${args[@]:3}"
 
     generate_keyfile ${pubkey} ${keyfile} ${enckeyfile}
-    hd ${keyfile} | head -n 2
-    aesencrypt ${keyfile} ${encfilename} "${files[@]}"
+    aes_encrypt ${keyfile} ${encfilename} "${files[@]}"
     # clean up the keyfile
     rm ${keyfile}
 }
 
-function aesdecrypt () {
-    args=( $@ )
-    keyfile="${args[0]}"
-    encfilename="${args[1]}"
-    files="${args[@]:2}"
-
-    if [[ ! -e ${keyfile} ]];
-    then
-        echo "[decrypt] missing keyfile (${keyfile}). make sure to call decrypt_keyfile() first"
-        return 1
-    fi
-
-    # decrypt and decompress file with keyfiles
-    openssl enc -d -aes-256-cbc -in ${encfilename} -pass file:${keyfile} | tar xz "${files[@]}"
-}
 
 function decrypt () {
     args=( $@ )
@@ -72,7 +92,7 @@ function decrypt () {
     files="${args[@]:3}"
 
     decrypt_keyfile ${privkey} ${enckeyfile} ${keyfile}
-    aesdecrypt ${keyfile} ${encfilename} "${files[@]}"
+    aes_decrypt ${keyfile} ${encfilename} "${files[@]}"
     # clean up the keyfile
     rm ${keyfile}
  }
